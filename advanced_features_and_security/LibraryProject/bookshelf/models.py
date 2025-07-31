4ea0735d92c39db26dac12ab104df7471cadf4a8
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-07-31 20:39:38 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+class CustomUser(AbstractUser):, date_of_birth, profile_photo
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
