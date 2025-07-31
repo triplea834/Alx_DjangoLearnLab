cc60ae5af65b993120a0db43874664d03c7d3a2c
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-07-31 20:40:48 +0100

    Update models.py1

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class CustomUser(AbstractUser):, date_of_birth, profile_photo
+class CustomUserManager(BaseUserManager):, create_user, create_superuser
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
