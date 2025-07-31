21cb4404f1069049050edba413f5b4210b380772
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-07-31 19:42:11 +0100

    Update urls.py1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_book, name='add_book'),
-    path('edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('add/', views.add_book, name='add_book/'),
+    path('edit/<int:pk>/', views.edit_book, name='edit_book/'),
+    path('delete/<int:pk>/', views.delete_book, name='delete_book/'),
 ]
 
