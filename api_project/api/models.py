a1e66f38aa6543aca80f338009dbb6cf5f199895
Adedotun <adeadeny5@gmail.com>
2025-08-01 02:10:44 +0100

    Implement BookViewSet with DRF CRUD operations using routers

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.titles
+        return self.title
+    
