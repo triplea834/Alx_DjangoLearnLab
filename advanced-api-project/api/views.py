f01cf670c21c8f1d6997bea179505180537819c0
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-08-09 21:52:50 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 
 
 class BookList(generics.ListAPIView):
