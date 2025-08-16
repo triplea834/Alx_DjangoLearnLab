abd72ae8c12b71634e08863e679664eb7742950c
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-08-16 15:44:56 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,7 +3,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
-from .models import Comment
+from .models import Comment 
+# TagWidget()", widgets
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
