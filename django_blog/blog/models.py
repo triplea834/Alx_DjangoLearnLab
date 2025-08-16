f0ee38bc609142915a6c4b193004e44c8beecb76
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-08-16 10:19:44 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
